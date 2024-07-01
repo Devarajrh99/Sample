7f088c3cc722b5ec3ce832050be04ed8b9d71573
Rudreshbsgowda <rudreshbs19@gmail.com>
2024-07-01 22:58:01 +0530

    My contribution

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <p>Welcome to new app</p>
+        <h1> hello I am H1</h1>
         <a
           className="App-link"
           href="https://reactjs.org"
